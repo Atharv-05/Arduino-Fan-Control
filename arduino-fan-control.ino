9d0d8e56b5b91030c3ed0b6b5f45e803fca1f751
Atharv-05 <71598247+Atharv-05@users.noreply.github.com>
2025-09-13 19:33:15 +0530

    Update arduino-fan-control.ino

--- arduino-fan-control.ino
+++ arduino-fan-control.ino
@@ -1,35 +1,26 @@
-#include <Arduino.h>
+int ntcPin = A0;    // NTC sensor
+int fanPin = 13;    // Fan (LED on D13)
 
-const int ntcPin = A0;    // NTC input
-const int fanPin = 13;    // Fan simulated by LED
-const float R0 = 10000;   // 10k reference resistor
-const float BETA = 3950;  // Beta value
-const float T0 = 298.15;  // 25°C in Kelvin
+float R0 = 10000;   // 10k resistor
+float BETA = 3950;  // Beta constant
+float T0 = 298.15;  // 25C in Kelvin
 
-unsigned long lastToggle = 0;
-int pwmState = LOW;
-int dutyCycle = 0; // 0 = off, 50 = half, 100 = full
+int fanSpeed = 0;   // 0 = off, 50 = half, 100 = full
 
-float readTemperature() {
+float readTemp() {
   int adc = analogRead(ntcPin);
   float Rntc = R0 * (1023.0 / adc - 1.0);
   float tempK = 1.0 / (1.0 / T0 + (1.0 / BETA) * log(Rntc / R0));
-  return tempK - 273.15; // Convert to Celsius
+  return tempK - 273.15; // in C
 }
 
-void softwarePWM(int duty) {
-  // duty = 0..100
-  static unsigned long lastMillis = 0;
+void softPWM(int duty) {
+  // simple software PWM
   unsigned long now = micros();
-
-  int period = 10000; // 10ms period => 100Hz PWM
+  int period = 10000; // 100Hz
   int onTime = period * duty / 100;
-
-  if (now % period < onTime) {
-    digitalWrite(fanPin, HIGH);
-  } else {
-    digitalWrite(fanPin, LOW);
-  }
+  if (now % period < onTime) digitalWrite(fanPin, HIGH);
+  else digitalWrite(fanPin, LOW);
 }
 
 void setup() {
@@ -38,23 +29,20 @@ void setup() {
 }
 
 void loop() {
-  float tempC = readTemperature();
+  float tempC = readTemp();
 
-  if (tempC < 25) {
-    dutyCycle = 0;
-  } else if (tempC < 30) {
-    dutyCycle = 50;
-  } else {
-    dutyCycle = 100;
-  }
+  if (tempC < 25) fanSpeed = 0;
+  else if (tempC < 30) fanSpeed = 50;
+  else fanSpeed = 100;
 
-  softwarePWM(dutyCycle);
+  softPWM(fanSpeed);
 
-  Serial.print("Temperature: ");
+  // print values
+  Serial.print("Temp=");
   Serial.print(tempC);
-  Serial.print(" °C | Fan: ");
-  if (dutyCycle == 0) Serial.println("OFF");
-  else if (dutyCycle == 50) Serial.println("50%");
+  Serial.print(" C | Fan=");
+  if (fanSpeed == 0) Serial.println("OFF");
+  else if (fanSpeed == 50) Serial.println("50%");
   else Serial.println("100%");
 
   delay(200);
